958563927e0b38c8f83add87476b055c7ea49e95
rajyavardhanbithale <bithale03@gmail.com>
2024-01-02 20:23:14 +0530

    v2.r8

---
+++ src/app/components/Images.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+
+interface ImagesProps {
+  theme: string;
+}
+
+export default function Images(props: ImagesProps) {
+  return (
+    <>
+      {/* <img src={`images/${props.theme}/inner.webp`} className="absolute w-[30%] z-40 spinning-object1 " />
+      <img src={`images/${props.theme}/middle.webp`} className="absolute w-[43%] z-30 spinning-object2 " />
+      <img src={`images/${props.theme}/outer.webp`} className="absolute w-[53%] z-10  spinning-object3 " /> */}
+      {/* <img src={`images/${props.theme}/background.webp`} className="" /> */}
+
+      {/* <Image src={`/images/${props.theme}/background.webp`} height={100} width={200}></Image> */}
+
+      {/* <Image src={`/images/${props.theme}/background.webp`} height={100} width={200}></Image> */}
+
+      <Image
+        src={`/images/${props.theme}/inner.webp`}
+        width={0}
+        height={0}
+        alt="inner"
+        sizes="100vw"
+        style={{ width: '30%', height: 'auto' }}
+        className="absolute z-40 spinning-object1"
+      />
+
+      <Image
+        src={`/images/${props.theme}/middle.webp`}
+        width={0}
+        height={0}
+        alt="middle"
+        sizes="100vw"
+        style={{ width: '43%', height: 'auto' }}
+        className="absolute z-30 spinning-object2"
+      />
+
+      <Image
+        src={`/images/${props.theme}/outer.webp`}
+        width={0}
+        height={0}
+        alt="outer"
+        sizes="100vw"
+        style={{ width: '53%', height: 'auto' }}
+        className="absolute z-10 spinning-object3"
+      />
+
+      <Image
+        src={`/images/${props.theme}/background.webp`}
+        width={0}
+        height={0}
+        alt="outer"
+        sizes="100vw"
+        style={{ width: '100%', height: 'auto' }}
+      />
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { IoPaperPlane } from "react-icons/io5";
+'use client'
 import { ImRadioChecked2 } from "react-icons/im";
-
-
 import { ImRadioUnchecked } from "react-icons/im";
-
 import { Major_Mono_Display } from 'next/font/google';
+import { useState } from "react";
+import Images from "./components/Images";
 
 
 const MMD = Major_Mono_Display({
@@ -13,9 +12,18 @@ const MMD = Major_Mono_Display({
 })
 
 export default function Home() {
-  const theme = 'forest'
-  const theme_count = 3
+  const [selectedTheme, setSelectedTheme] = useState('forest')
+  const [seed, setSeed] = useState(1);
 
+  const availableThemes: string[] = ['forest','desert','beach']
+  const theme: string = selectedTheme
+  const themeCount: number = availableThemes.length
+  const themeActive: number = availableThemes.indexOf(theme)
+
+  const handleThemeUpdate = (themeIndex:number) => {
+    setSelectedTheme(availableThemes[themeIndex])
+    setSeed(Math.random());
+  }
 
   return (
     <>
@@ -43,21 +51,27 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col gap-5 items-start p-4 justify-center w-full min-h-screen absolute z-50 -mt-36 lg:-mt-0">
-        <ImRadioChecked2  className="text-white lg:text-2xl text-lg font-bold" />
+        <ImRadioChecked2 className="text-white lg:text-2xl text-lg font-bold" />
         <ImRadioUnchecked className="text-white lg:text-2xl text-lg font-bold" />
         <ImRadioUnchecked className="text-white lg:text-2xl text-lg font-bold" />
         <ImRadioUnchecked className="text-white lg:text-2xl text-lg font-bold" />
         <div>
-          <img src="images/help_arrow.png" className="absolute w-[5%] left-[4.5%] top-[43%] lg:w-[5%] lg:left-[2.5%] lg:top-[39%]" />
+          <img src="images/help_arrow.png" className="absolute w-[5%] left-[4.5%] top-[43%] lg:w-[5%] lg:left-[2.5%] lg:top-[39%] animate-pulse" />
           <span className="text-base tracking-tighter lg:tracking-tighter lg:text-2xl text-white absolute left-[10%] top-[41%] underline lg:left-[8%] lg:top-[36%] drop-shadow-2xl">Click Here To Navigate</span>
         </div>
       </div>
 
+      <div className="flex flex-row gap-5 items-end p-8 justify-center w-full min-h-screen absolute z-50 -mt-36 lg:-mt-0">
+
+        {[...Array(themeCount)].map((_, index) => (
+          <div onClick={()=>handleThemeUpdate(index)} key={index} className={`h-2 w-10 rounded-full bg-white ${index===themeActive ? "opacity-100" : "opacity-60"} hover:opacity-100 transition duration-500`}></div>
+        ))}
+      </div>
+
+
+
       <div className="flex items-center z-0 justify-center w-full lg:min-h-screen">
-        <img src={`images/${theme}/inner.webp`} className="absolute w-[30%] z-40 spinning-object1 " />
-        <img src={`images/${theme}/middle.webp`} className="absolute w-[43%] z-30 spinning-object2 " />
-        <img src={`images/${theme}/outer.webp`} className="absolute w-[53%] z-10  spinning-object3 " />
-        <img src={`images/${theme}/background.webp`} className="" />
+          <Images theme={selectedTheme} key={seed}></Images>
       </div>
     </>
   )
